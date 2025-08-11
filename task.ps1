7525828f9f9a4f3e5eef4963b9f916b2fb734f3e
Illia Bilous <krabbys1111@gmail.com>
2025-08-12 02:18:34 +0300

    update task script to create multiple VMs with Data Collection Rule and save deployment info

--- task.ps1
+++ task.ps1
@@ -1,70 +1,128 @@
+# Параметри
 $location = "uksouth"
-$resourceGroupName = "mate-resources"
+$resourceGroupName = "mate-azure-task-13"  # Оновлено на правильне ім’я
 $networkSecurityGroupName = "defaultnsg"
 $virtualNetworkName = "vnet"
 $subnetName = "default"
 $vnetAddressPrefix = "10.0.0.0/16"
 $subnetAddressPrefix = "10.0.0.0/24"
 $sshKeyName = "linuxboxsshkey"
-$sshKeyPublicKey = Get-Content "~/.ssh/id_rsa.pub" 
-$publicIpAddressName = "linuxboxpip"
-$vmName = "matebox"
-$vmImage = "Ubuntu2204"
+$sshKeyPublicKey = Get-Content "~/.ssh/id_rsa.pub"
+
+$publicIpAddressNamePrefix = "linuxboxpip"
+$vmNamePrefix = "matebox"
+$vmImage = "UbuntuLTS"
 $vmSize = "Standard_B1s"
-$dnsLabel = "matetask" + (Get-Random -Count 1) 
+$dnsLabelPrefix = "matetask"
 
-Write-Host "Creating a resource group $resourceGroupName ..."
-New-AzResourceGroup -Name $resourceGroupName -Location $location
+$vmCount = 3
+$githubUsername = "mate-academy"
 
-Write-Host "Creating a network security group $networkSecurityGroupName ..."
-$nsgRuleSSH = New-AzNetworkSecurityRuleConfig -Name SSH  -Protocol Tcp -Direction Inbound -Priority 1001 -SourceAddressPrefix * -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange 22 -Access Allow;
-$nsgRuleHTTP = New-AzNetworkSecurityRuleConfig -Name HTTP  -Protocol Tcp -Direction Inbound -Priority 1002 -SourceAddressPrefix * -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange 8080 -Access Allow;
+# Створення NSG та мережі (поза циклом)
+Write-Host "Creating network security group $networkSecurityGroupName ..."
+$nsgRuleSSH = New-AzNetworkSecurityRuleConfig -Name SSH -Protocol Tcp -Direction Inbound -Priority 1001 -SourceAddressPrefix * -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange 22 -Access Allow
+$nsgRuleHTTP = New-AzNetworkSecurityRuleConfig -Name HTTP -Protocol Tcp -Direction Inbound -Priority 1002 -SourceAddressPrefix * -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange 8080 -Access Allow
 New-AzNetworkSecurityGroup -Name $networkSecurityGroupName -ResourceGroupName $resourceGroupName -Location $location -SecurityRules $nsgRuleSSH, $nsgRuleHTTP
 
-Write-Host "Creating a virtual network ..."
 $subnet = New-AzVirtualNetworkSubnetConfig -Name $subnetName -AddressPrefix $subnetAddressPrefix
 New-AzVirtualNetwork -Name $virtualNetworkName -ResourceGroupName $resourceGroupName -Location $location -AddressPrefix $vnetAddressPrefix -Subnet $subnet
 
-Write-Host "Creating a SSH key ..."
+# Створення SSH ключа
 New-AzSshKey -Name $sshKeyName -ResourceGroupName $resourceGroupName -PublicKey $sshKeyPublicKey
 
-Write-Host "Creating a Public IP Address ..."
+# --- Створення Data Collection Rule (DCR) ---
+$dcrName = "mateDCR"
+$dcrDescription = "Data Collection Rule for OS-level metrics"
 
-New-AzPublicIpAddress -Name $publicIpAddressName -ResourceGroupName $resourceGroupName -Location $location -Sku Standard -AllocationMethod Static -DomainNameLabel $dnsLabel
-Write-Host "Creating a VM ..."
-# Update the VM deployment command to enable a system-assigned mannaged identity on it. 
-New-AzVm `
--ResourceGroupName $resourceGroupName `
--Name $vmName `
--Location $location `
--image $vmImage `
--size $vmSize `
--SubnetName $subnetName `
--VirtualNetworkName $virtualNetworkName `
--SecurityGroupName $networkSecurityGroupName `
--SshKeyName $sshKeyName `
--PublicIpAddressName $publicIpAddressName `
--SystemAssignedIdentity
-
-Write-Host "Installing the TODO web app..."
-$Params = @{
-    ResourceGroupName  = $resourceGroupName
-    VMName             = $vmName
-    Name               = 'CustomScript'
-    Publisher          = 'Microsoft.Azure.Extensions'
-    ExtensionType      = 'CustomScript'
-    TypeHandlerVersion = '2.1'
-    Settings          = @{fileUris = @('https://raw.githubusercontent.com/mate-academy/azure_task_13_vm_monitoring/main/install-app.sh'); commandToExecute = './install-app.sh'}
+# Визначення DCR (приклад для збору базових метрик ОС)
+$dcrDataSources = @{
+    performanceCounters = @(
+        @{ counterSpecifier = "\\Processor(_Total)\\% Processor Time"; samplingFrequencyInSeconds = 15 }
+        @{ counterSpecifier = "\\Memory\\Available MBytes"; samplingFrequencyInSeconds = 15 }
+    )
 }
-Set-AzVMExtension @Params
+$dcrDestinations = @{
+    logAnalytics = @{
+        workspaceResourceId = "/subscriptions/$((Get-AzContext).Subscription.Id)/resourceGroups/$resourceGroupName/providers/Microsoft.OperationalInsights/workspaces/mateWorkspace"
+    }
+}
 
-# Install Azure Monitor Agent VM extention -> 
-Set-AzVMExtension `
--Name AzureMonitorLinuxAgent `
--ExtensionType AzureMonitorLinuxAgent `
--Publisher Microsoft.Azure.Monitor `
--ResourceGroupName $resourceGroupName `
--VMName $vmName `
--Location $location `
--TypeHandlerVersion "1.19" `
--EnableAutomaticUpgrade $true
+# Якщо у вас немає Log Analytics workspace, створіть або змініть цей параметр відповідно.
+
+# Перевіряємо чи існує DCR, якщо ні – створюємо
+if (-not (Get-AzDataCollectionRule -ResourceGroupName $resourceGroupName -Name $dcrName -ErrorAction SilentlyContinue)) {
+    Write-Host "Creating Data Collection Rule $dcrName ..."
+    New-AzDataCollectionRule -ResourceGroupName $resourceGroupName -Name $dcrName `
+        -Location $location `
+        -DataSources $dcrDataSources `
+        -Destinations $dcrDestinations `
+        -Description $dcrDescription
+} else {
+    Write-Host "Data Collection Rule $dcrName already exists."
+}
+
+# Масив для результатів
+$results = @()
+
+for ($i=1; $i -le $vmCount; $i++) {
+    $vmName = "$vmNamePrefix$i"
+    $publicIpAddressName = "$publicIpAddressNamePrefix$i"
+    $dnsLabel = "$dnsLabelPrefix$i$(Get-Random -Maximum 9999)"
+
+    Write-Host "Creating public IP $publicIpAddressName with DNS label $dnsLabel ..."
+    New-AzPublicIpAddress -Name $publicIpAddressName -ResourceGroupName $resourceGroupName -Location $location -Sku Standard -AllocationMethod Static -DomainNameLabel $dnsLabel
+
+    Write-Host "Creating VM $vmName with System Assigned Identity..."
+    New-AzVm `
+        -ResourceGroupName $resourceGroupName `
+        -Name $vmName `
+        -Location $location `
+        -Image $vmImage `
+        -Size $vmSize `
+        -SubnetName $subnetName `
+        -VirtualNetworkName $virtualNetworkName `
+        -SecurityGroupName $networkSecurityGroupName `
+        -SshKeyName $sshKeyName `
+        -PublicIpAddressName $publicIpAddressName `
+        -SystemAssignedIdentity
+
+    $scriptUri = "https://raw.githubusercontent.com/$githubUsername/azure_task_13_vm_monitoring/main/install-app.sh"
+
+    $params = @{
+        ResourceGroupName = $resourceGroupName
+        VMName = $vmName
+        Name = 'CustomScript'
+        Publisher = 'Microsoft.Azure.Extensions'
+        ExtensionType = 'CustomScript'
+        TypeHandlerVersion = '2.1'
+        Settings = @{
+            fileUris = @($scriptUri);
+            commandToExecute = './install-app.sh'
+        }
+    }
+    Write-Host "Setting CustomScript extension on $vmName ..."
+    Set-AzVMExtension @params
+
+    # Отримуємо VM об'єкт, щоб отримати ідентифікатор для DCR асоціації
+    $vm = Get-AzVM -ResourceGroupName $resourceGroupName -Name $vmName
+
+    # Асоціюємо DCR з VM
+    Write-Host "Associating Data Collection Rule $dcrName with VM $vmName ..."
+    New-AzDataCollectionRuleAssociation -ResourceGroupName $resourceGroupName `
+        -Name "${vmName}DcrAssociation" `
+        -DataCollectionRuleId $dcr.Id `
+        -Scope $vm.Id
+
+    # Отримуємо публічну IP та DNS
+    $publicIp = Get-AzPublicIpAddress -ResourceGroupName $resourceGroupName -Name $publicIpAddressName
+
+    $results += [PSCustomObject]@{
+        VMName = $vmName
+        PublicIpAddress = $publicIp.IpAddress
+        DnsName = $publicIp.DnsSettings.Fqdn
+    }
+}
+
+# Записуємо у result.json
+$results | ConvertTo-Json -Depth 3 | Out-File -FilePath "./result.json" -Encoding utf8
+Write-Host "Saved deployment info to result.json"
